89f8bfb8c63736ba8353ee6eb395c9f1be3a4ed7
Lempek <37885098+LempekPL@users.noreply.github.com>
2026-03-09 02:14:27 +0100

    Refactor directory walking

--- nob.c
+++ nob.c
@@ -1,27 +1,19 @@
-#include <stdio.h>
 #define NOB_IMPLEMENTATION
 #include "nob.h"
-#include <string.h>
-#include <stdio.h>
 
 #define BUILD_FOLDER "build/"
 #define SRC_FOLDER "src/"
 
-void walk_the_dir(Nob_Cmd* cmd, char* name) {
-    DIR* dir = opendir(name);
-    struct dirent* entry;
-    while ((entry = readdir(dir)) != NULL) {
-        size_t lens = sizeof(char)*(strlen(name)+strlen(entry->d_name)+2);
-        char* new_name = malloc(lens);
-        snprintf(new_name, lens, "%s/%s", name, entry->d_name);
-        if (entry->d_name[0] == '.') continue;
-        if (entry->d_type == 4) {
-            walk_the_dir(cmd, new_name);
-        } else {
-            nob_cc_inputs(cmd, new_name);
-        }
+bool add_all_files(Nob_Walk_Entry entry) {
+    Nob_Cmd* cmd = (Nob_Cmd*)entry.data;
+    if (entry.type != NOB_FILE_DIRECTORY) {
+        size_t strlength = strlen(entry.path) + 1;
+        char* new_name = malloc(strlength);
+        if (new_name == NULL) return false;
+        strcpy_s(new_name, strlength, entry.path);
+        nob_cc_inputs(cmd, new_name);
     }
-    closedir(dir);
+    return true;
 }
 
 int main(int argc, char** argv) {
@@ -34,12 +26,12 @@ int main(int argc, char** argv) {
     nob_cc(&cmd);
     nob_cc_flags(&cmd);
     nob_cc_output(&cmd, BUILD_FOLDER "cimpl");
-    walk_the_dir(&cmd, "src");
+
+    if (!nob_walk_dir("src", add_all_files, &cmd)) return 1;
     if (!nob_cmd_run(&cmd)) return 1;
 
     nob_cmd_append(&cmd, BUILD_FOLDER"cimpl");
     if (!nob_cmd_run(&cmd)) return 1;
-
 
     return 0;
 }

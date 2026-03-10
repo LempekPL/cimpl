67d58cbc20aa92c9e5fa1a8bdce4603bc04f1a4e
LempekPL <37885098+LempekPL@users.noreply.github.com>
2026-03-10 14:49:17 +0100

    Easier debug building

--- nob.c
+++ nob.c
@@ -23,17 +23,29 @@ bool add_all_files(Nob_Walk_Entry entry) {
 int main(int argc, char** argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
 
+    bool debug = false;
+    bool gdb = false;
+    bool gf = false;
+    if (argc > 1) {
+        gdb = strcmp(argv[1], "gdb") == 0;
+        gf = strcmp(argv[1], "gf") == 0;
+        debug = gdb || gf || strcmp(argv[1], "debug") == 0;
+    }
+
     if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
 
     Nob_Cmd cmd = {0};
 
     nob_cc(&cmd);
     nob_cc_flags(&cmd);
+    if (debug) nob_cmd_append(&cmd, "-g");
     nob_cc_output(&cmd, BUILD_FOLDER "cimpl");
 
     if (!nob_walk_dir("src", add_all_files, &cmd)) return 1;
     if (!nob_cmd_run(&cmd)) return 1;
 
+    if (gdb) nob_cmd_append(&cmd, "gdb");
+    if (gf) nob_cmd_append(&cmd, "gf2");
     nob_cmd_append(&cmd, BUILD_FOLDER"cimpl");
     if (!nob_cmd_run(&cmd)) return 1;
 
